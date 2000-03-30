58174f2226bdd7f26dd41a62505130f6969e956c
Michael A. MacDonald <michaelm@antlersoft.com>
2000-03-30 03:48:11 +0000

    Add history list

--- eclipse-projects/bbq_eclipse/src/com/antlersoft/analyzer/UIQuery.java
+++ eclipse-projects/bbq_eclipse/src/com/antlersoft/analyzer/UIQuery.java
@@ -29,134 +29,138 @@ public class UIQuery
     QueryParser qp;
     JTextArea queryArea;
     JTextArea outputArea;
+    HistoryList historyList;
     Window frameWindow;
 
     UIQuery()
     {
-	analyzerDB=new ObjectAnalyzeDB();
-	qp=new QueryParser();
-	chooser=new JFileChooser();
-	chooser.setFileSelectionMode( JFileChooser.FILES_AND_DIRECTORIES);
-	//chooser.setMultiSelectionEnabled( true); // Doesn't work yet
-	//ExtensionFileFilter filter=new ExtensionFileFilter();
-	//filter.addExtension( "class");
-	//filter.addExtension( "zip");
-	//filter.addExtension( "jar");
-	//chooser.setFileFilter( filter);
+        analyzerDB=new ObjectAnalyzeDB();
+        qp=new QueryParser();
+        chooser=new JFileChooser();
+        chooser.setFileSelectionMode( JFileChooser.FILES_AND_DIRECTORIES);
+        //chooser.setMultiSelectionEnabled( true); // Doesn't work yet
+        //ExtensionFileFilter filter=new ExtensionFileFilter();
+        //filter.addExtension( "class");
+        //filter.addExtension( "zip");
+        //filter.addExtension( "jar");
+        //chooser.setFileFilter( filter);
     }
 
     Component createComponents()
     {
-	// Create components
-	JButton queryButton=new JButton( "Query");
-	Dimension buttonDimension=queryButton.getPreferredSize();
-	queryButton.setMinimumSize( buttonDimension);
-	queryButton.setMaximumSize( buttonDimension);
-	queryArea=new JTextArea( 2, 60);
-	JScrollPane queryScroll=new JScrollPane( queryArea);
-	JScrollPane storedArea=new JScrollPane( new StoredValuesList());
-	Box upperPane=Box.createHorizontalBox();
-	upperPane.add( queryButton);
-	upperPane.add( queryScroll);
-	upperPane.add( storedArea);
-	outputArea=new JTextArea( 16, 80);
-	JScrollPane lowerPane=new JScrollPane( outputArea);
+        // Create components
+        JButton queryButton=new JButton( "Query");
+        Dimension buttonDimension=queryButton.getPreferredSize();
+        queryButton.setMinimumSize( buttonDimension);
+        queryButton.setMaximumSize( buttonDimension);
+        queryArea=new JTextArea( 2, 60);
+        JScrollPane queryScroll=new JScrollPane( queryArea);
+        historyList=new HistoryList( queryArea);
+        JScrollPane historyScroll=new JScrollPane( historyList);
+        JScrollPane storedArea=new JScrollPane( new StoredValuesList());
+        Box upperPane=Box.createHorizontalBox();
+        upperPane.add( queryButton);
+        upperPane.add( new JSplitPane( JSplitPane.VERTICAL_SPLIT, queryScroll, historyScroll));
+        upperPane.add( storedArea);
+        outputArea=new JTextArea( 16, 80);
+        JScrollPane lowerPane=new JScrollPane( outputArea);
         JSplitPane mainPane=new JSplitPane( JSplitPane.VERTICAL_SPLIT, upperPane, lowerPane);
 
-	// Create actions
-	outputArea.setEditable( false);
-	queryButton.addActionListener( new ActionListener() {
-		public void actionPerformed( ActionEvent ae)
-		{
-		    try
-		    {
-			String line=queryArea.getText();
-			if ( line==null || line.length()==0)
-			    return;
-			qp.setLine( line);
-			SetExpression se=qp.getExpression();
-			Enumeration e=se.execute( analyzerDB);
-			TreeSet resultSorter=new TreeSet();
-			while ( e.hasMoreElements())
-			    resultSorter.add( e.nextElement().toString());
-			Iterator iterator=resultSorter.iterator();
-			StringBuffer results=new StringBuffer();
-			while ( iterator.hasNext())
-			{
-			    results.append( (String)iterator.next());
-			    results.append( '\n');
-			}
-			outputArea.setText( results.toString());
-		    }
-		    catch ( ParseException pe)
-		    {
-			displayException( "Parse Error", pe);
-		    }
-		    catch ( Exception unkn)		    
-		    {
-			displayException( "Query Error", unkn);
-		    }
-		}
-	    });
+        // Create actions
+        outputArea.setEditable( false);
+        queryButton.addActionListener( new ActionListener() {
+                public void actionPerformed( ActionEvent ae)
+                {
+                    try
+                    {
+                        String line=queryArea.getText();
+                        if ( line==null || line.length()==0)
+                            return;
+                        qp.setLine( line);
+                        historyList.addQuery( line);
+                        SetExpression se=qp.getExpression();
+                        Enumeration e=se.execute( analyzerDB);
+                        TreeSet resultSorter=new TreeSet();
+                        while ( e.hasMoreElements())
+                            resultSorter.add( e.nextElement().toString());
+                        Iterator iterator=resultSorter.iterator();
+                        StringBuffer results=new StringBuffer();
+                        while ( iterator.hasNext())
+                        {
+                            results.append( (String)iterator.next());
+                            results.append( '\n');
+                        }
+                        outputArea.setText( results.toString());
+                    }
+                    catch ( ParseException pe)
+                    {
+                        displayException( "Parse Error", pe);
+                    }
+                    catch ( Exception unkn)
+                    {
+                        displayException( "Query Error", unkn);
+                    }
+                }
+            });
 
-	return mainPane;
+        return mainPane;
     }
 
     JMenuBar createMenuBar()
     {
         JMenuBar menuBar=new JMenuBar();
 
-	// Create File menu
-	JMenu fileMenu=new JMenu( "File");
+        // Create File menu
+        JMenu fileMenu=new JMenu( "File");
 
-	// Create analyze action
-	fileMenu.add( new AnalyzeAction());
+        // Create analyze action
+        fileMenu.add( new AnalyzeAction());
 
-	// Create save action
-	fileMenu.add( new SaveAction());
+        // Create save action
+        fileMenu.add( new SaveAction());
 
-	// Create Exit action
-	fileMenu.addSeparator();
-	fileMenu.add( new ExitAction());
+        // Create Exit action
+        fileMenu.addSeparator();
+        fileMenu.add( new ExitAction());
 
-	// Add menu to menu bar
-	menuBar.add( fileMenu);
+        // Add menu to menu bar
+        menuBar.add( fileMenu);
 
-	return menuBar;	
+        return menuBar;
     }
 
     void displayException( String caption, Throwable t)
     {
-	StringWriter sw=new StringWriter( 1000);
-	PrintWriter pw=new PrintWriter( sw);
-	t.printStackTrace( pw);
-	pw.close();
-	JOptionPane.showMessageDialog( frameWindow, t.getMessage()+" \n"+sw.toString(),
-	    caption, JOptionPane.ERROR_MESSAGE);
+        StringWriter sw=new StringWriter( 1000);
+        PrintWriter pw=new PrintWriter( sw);
+        t.printStackTrace( pw);
+        pw.close();
+        JOptionPane.showMessageDialog( frameWindow, t.getMessage()+" \n"+sw.toString(),
+            caption, JOptionPane.ERROR_MESSAGE);
     }
 
     public static void main( String argv[])
-	throws Exception
+        throws Exception
     {
-	final UIQuery app = new UIQuery();
-	app.analyzerDBOpenString=argv[0];
-	app.analyzerDB.openDB( app.analyzerDBOpenString);
+        final UIQuery app = new UIQuery();
+        app.analyzerDBOpenString=argv[0];
+        app.analyzerDB.openDB( app.analyzerDBOpenString);
 
-	JFrame appFrame=new JFrame( "Querying "+argv[0]);
-	app.frameWindow=appFrame;
+        JFrame appFrame=new JFrame( "Querying "+argv[0]);
+        app.frameWindow=appFrame;
         Component contents = app.createComponents();
         appFrame.getContentPane().add(contents, BorderLayout.CENTER);
-	appFrame.setJMenuBar( app.createMenuBar());
+        appFrame.setJMenuBar( app.createMenuBar());
         //Finish setting up the frame, and show it.
         appFrame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
-		try
-		{
-		    app.analyzerDB.closeDB();
-		}
-		catch ( Exception exception)
-		{
-		}
+                try
+                {
+                    app.analyzerDB.closeDB();
+                }
+                catch ( Exception exception)
+                {
+                }
                 System.exit(0);      }        });
         appFrame.pack();
         appFrame.setVisible(true);
@@ -164,92 +168,144 @@ public class UIQuery
 
     class ExitAction extends AbstractAction
     {
-	ExitAction()
-	{
-	    super( "Exit");
-	}
+        ExitAction()
+        {
+            super( "Exit");
+        }
 
-	public void actionPerformed( ActionEvent event)
-	{
-	    frameWindow.dispatchEvent( new WindowEvent( frameWindow, WindowEvent.WINDOW_CLOSING));
-	}
+        public void actionPerformed( ActionEvent event)
+        {
+            frameWindow.dispatchEvent( new WindowEvent( frameWindow, WindowEvent.WINDOW_CLOSING));
+        }
     }
 
     class SaveAction extends AbstractAction
     {
-	SaveAction()
-	{
-	    super( "Save");
-	}
+        SaveAction()
+        {
+            super( "Save");
+        }
 
-	public void actionPerformed( ActionEvent event)
-	{
-	    if ( analyzerDB!=null)
-	    {
-		try
-		{
+        public void actionPerformed( ActionEvent event)
+        {
+            if ( analyzerDB!=null)
+            {
+                try
+                {
                     analyzerDB.closeDB();
-		    analyzerDB.openDB( analyzerDBOpenString);
-		}
-		catch ( Exception e)
-		{
-		    displayException( "Save Error", e);
-		}
-	    }
-	}
+                    analyzerDB.openDB( analyzerDBOpenString);
+                }
+                catch ( Exception e)
+                {
+                    displayException( "Save Error", e);
+                }
+            }
+        }
     }
 
     class AnalyzeAction extends AbstractAction
     {
-	AnalyzeAction()
-	{
-	    super( "Analyze");
-	}
+        AnalyzeAction()
+        {
+            super( "Analyze");
+        }
 
-	public void actionPerformed( ActionEvent event)
-	{
-	    try
-	    {
-		if ( chooser.showDialog( frameWindow, "Analyze")==JFileChooser.APPROVE_OPTION)
-		{
-		    Cursor oldCursor=frameWindow.getCursor();
-		    frameWindow.setCursor( Cursor.getPredefinedCursor( Cursor.WAIT_CURSOR));
-		    try
-		    {
-			// File[] selectedFiles=chooser.getSelectedFiles();
-			// Multi-file selection is broken
-			File[] selectedFiles=new File[] { chooser.getSelectedFile() };
+        public void actionPerformed( ActionEvent event)
+        {
+            try
+            {
+                if ( chooser.showDialog( frameWindow, "Analyze")==JFileChooser.APPROVE_OPTION)
+                {
+                    Cursor oldCursor=frameWindow.getCursor();
+                    frameWindow.setCursor( Cursor.getPredefinedCursor( Cursor.WAIT_CURSOR));
+                    try
+                    {
+                        // File[] selectedFiles=chooser.getSelectedFiles();
+                        // Multi-file selection is broken
+                        File[] selectedFiles=new File[] { chooser.getSelectedFile() };
 
-			for ( int i=0; i<selectedFiles.length; i++)
-			{
-			    DBClass.addFileToDB( selectedFiles[i], analyzerDB);
-			}
-		    }
-		    finally
-		    {
-			frameWindow.setCursor( oldCursor);
-		    }
-		}
-	    }
-	    catch ( Exception e)
-	    {
-		displayException( "Analyze Error", e);
-	    }
-	}
+                        for ( int i=0; i<selectedFiles.length; i++)
+                        {
+                            DBClass.addFileToDB( selectedFiles[i], analyzerDB);
+                        }
+                    }
+                    finally
+                    {
+                        frameWindow.setCursor( oldCursor);
+                    }
+                }
+            }
+            catch ( Exception e)
+            {
+                displayException( "Analyze Error", e);
+            }
+        }
     }
 
     class StoredValuesList extends JList implements PropertyChangeListener
     {
-	StoredValuesList()
-	{
-	    super();
-	    qp.addStoredValuesListener( this);
-	    setListData( qp.getStoredValues());
-	}
+        StoredValuesList()
+        {
+            super();
+            qp.addStoredValuesListener( this);
+            setVisibleRowCount(2);
+            setListData( qp.getStoredValues());
+        }
 
-	public void propertyChange( PropertyChangeEvent pce)
-	{
-	    setListData( qp.getStoredValues());
-	}
+        public void propertyChange( PropertyChangeEvent pce)
+        {
+            setListData( qp.getStoredValues());
+        }
+    }
+
+    class HistoryList extends JList
+    {
+    	private DefaultListModel dlm;
+        private JTextArea text;
+
+    	HistoryList( JTextArea toChange)
+        {
+        	super( new DefaultListModel());
+            text=toChange;
+            dlm=(DefaultListModel)getModel();
+            setVisibleRowCount(2);
+            addMouseListener( new MouseAdapter() {
+            	public void mouseClicked(MouseEvent e) {
+					if (e.getClickCount() == 2) {
+     					synchronized( dlm) {
+				        	int index = locationToIndex(e.getPoint());
+	            			text.setText( (String)dlm.elementAt(index));
+	                		moveToTop( index);
+                  		}
+			        }
+             	}
+            });
+        }
+
+        synchronized void addQuery( String toAdd)
+        {
+        	synchronized ( dlm)
+         	{
+          		int j=0;
+        		for ( Enumeration i=dlm.elements();
+          			i.hasMoreElements(); j++)
+             	{
+              		if ( toAdd.equals( i.nextElement()))
+                	{
+                 		moveToTop( j);
+                   		return;
+                 	}
+            	}
+             	dlm.insertElementAt( toAdd, 0);
+            }
+        }
+
+        private void moveToTop( int index)
+        {
+        	Object o=dlm.elementAt( index);
+         	dlm.remove( index);
+            dlm.insertElementAt( o, 0);
+            setSelectedIndex( 0);
+        }
     }
 }
