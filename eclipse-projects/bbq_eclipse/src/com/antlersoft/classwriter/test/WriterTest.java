8d3171a255bba25be9c9c6564be598e0f83d4149
Michael A. MacDonald <michaelm@antlersoft.com>
2000-08-17 03:23:59 +0000

    Moved test program to separate package so we can insure that enough things are public

---
+++ eclipse-projects/bbq_eclipse/src/com/antlersoft/classwriter/test/WriterTest.java
@@ -0,0 +1,25 @@
+
+/**
+ * Title:        <p>
+ * Description:  Java object database; also code analysis tool<p>
+ * Copyright:    Copyright (c) Michael MacDonald<p>
+ * Company:      <p>
+ * @author Michael MacDonald
+ * @version 1.0
+ */
+package classwriter.test;
+
+import java.io.*;
+import classwriter.*;
+
+public class WriterTest
+{
+    public static void main(String[] args)
+    	throws IOException
+    {
+    	ClassWriter writer=new ClassWriter();
+
+     	writer.readClass( new FileInputStream( args[0]));
+      	writer.writeClass( new FileOutputStream( args[1]));
+    }
+}
