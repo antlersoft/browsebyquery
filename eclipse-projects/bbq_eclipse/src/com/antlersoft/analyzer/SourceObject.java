97abaab153780909e4e9f99e5179b6af73134e11
Michael A. MacDonald <michaelm@antlersoft.com>
2000-09-28 21:53:55 +0000

    Interface implemented by browsable objects

---
+++ eclipse-projects/bbq_eclipse/src/com/antlersoft/analyzer/SourceObject.java
@@ -0,0 +1,17 @@
+
+/**
+ * Title:        antlersoft java software<p>
+ * Description:  antlersoft Moose
+ * antlersoft BBQ<p>
+ * Copyright:    Copyright (c) Michael MacDonald<p>
+ * Company:      antlersoft<p>
+ * @author Michael MacDonald
+ * @version 1.0
+ */
+package com.antlersoft.analyzer;
+
+public interface SourceObject
+{
+    public DBClass getDBClass();
+    public int getLineNumber();
+}
