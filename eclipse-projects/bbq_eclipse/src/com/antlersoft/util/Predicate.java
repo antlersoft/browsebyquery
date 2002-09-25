7a5316e9b3137fff04db5ef11bb6361c52393802
Michael A. MacDonald <michaelm@antlersoft.com>
2002-09-25 22:15:15 +0000

    Some stuff for additional statuses

---
+++ eclipse-projects/bbq_eclipse/src/com/antlersoft/util/Predicate.java
@@ -0,0 +1,15 @@
+package com.antlersoft.util;
+
+/**
+ * <p>Title: Warroom</p>
+ * <p>Description: Whiteboarding for WWIIOL</p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: antlersoft</p>
+ * @author Michael MacDonald
+ * @version 1.0
+ */
+
+public interface Predicate
+{
+    public boolean test( Object obj);
+}
