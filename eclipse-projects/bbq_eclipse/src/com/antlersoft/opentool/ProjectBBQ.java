edf3dcba360b0da6b995db4e6d74e549d52fb6e0
Michael A. MacDonald <michaelm@antlersoft.com>
2004-04-14 20:18:13 +0000

    Move common ui elements into new package under analyzer

--- eclipse-projects/bbq_eclipse/src/com/antlersoft/opentool/ProjectBBQ.java
+++ eclipse-projects/bbq_eclipse/src/com/antlersoft/opentool/ProjectBBQ.java
@@ -42,9 +42,13 @@ import java.awt.*;
 import java.awt.event.*;
 
 import com.antlersoft.analyzer.SourceObject;
+
 import com.antlersoft.analyzer.query.ParseException;
 import com.antlersoft.analyzer.query.QueryParser;
 import com.antlersoft.analyzer.query.SetExpression;
+
+import com.antlersoft.analyzer.ui.HistoryList;
+import com.antlersoft.analyzer.ui.StoredValuesList;
 
 public class ProjectBBQ implements NodeViewer
 {
@@ -165,7 +169,7 @@ public class ProjectBBQ implements NodeViewer
         buttonDimension.width*=3;
         queryScroll.setMinimumSize( buttonDimension);
         historyList=new HistoryList( queryArea);
-        storedValuesList=new StoredValuesList();
+        storedValuesList=new StoredValuesList( analyzer.qp);
         JScrollPane historyScroll=new JScrollPane( historyList);
         storedArea=new JScrollPane( storedValuesList);
         Box buttonBox=Box.createVerticalBox();
@@ -225,73 +229,6 @@ public class ProjectBBQ implements NodeViewer
             });
 
         return mainPane;
-    }
-
-    class StoredValuesList extends JList implements PropertyChangeListener
-    {
-        StoredValuesList()
-        {
-            super();
-            analyzer.qp.addStoredValuesListener( this);
-            setVisibleRowCount(6);
-            setListData( analyzer.qp.getStoredValues());
-        }
-
-        public void propertyChange( PropertyChangeEvent pce)
-        {
-            setListData( analyzer.qp.getStoredValues());
-        }
-    }
-
-    class HistoryList extends JList
-    {
-    	private DefaultListModel dlm;
-        private JTextArea text;
-
-    	HistoryList( JTextArea toChange)
-        {
-        	super( new DefaultListModel());
-            text=toChange;
-            dlm=(DefaultListModel)getModel();
-            setVisibleRowCount(2);
-            addMouseListener( new MouseAdapter() {
-            	public void mouseClicked(MouseEvent e) {
-					if (e.getClickCount() >1) {
-     					synchronized( dlm) {
-				        	int index = locationToIndex(e.getPoint());
-	            			text.setText( (String)dlm.elementAt(index));
-	                		moveToTop( index);
-                  		}
-			        }
-             	}
-            });
-        }
-
-        synchronized void addQuery( String toAdd)
-        {
-        	synchronized ( dlm)
-         	{
-          		int j=0;
-        		for ( Enumeration i=dlm.elements();
-          			i.hasMoreElements(); j++)
-             	{
-              		if ( toAdd.equals( i.nextElement()))
-                	{
-                 		moveToTop( j);
-                   		return;
-                 	}
-            	}
-             	dlm.insertElementAt( toAdd, 0);
-            }
-        }
-
-        private void moveToTop( int index)
-        {
-        	Object o=dlm.elementAt( index);
-         	dlm.remove( index);
-            dlm.insertElementAt( o, 0);
-            setSelectedIndex( 0);
-        }
     }
 
     class ResultArea extends JTextArea
