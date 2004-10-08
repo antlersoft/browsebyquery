30e8f55405e98cff8e1f7be05cbe924bd05ef450
Michael A. MacDonald <michaelm@antlersoft.com>
2004-10-08 17:08:52 +0000

    Use to create index hey generators on Object Reference fields

---
+++ eclipse-projects/bbq_eclipse/src/com/antlersoft/odb/ObjectRefKey.java
@@ -0,0 +1,38 @@
+package com.antlersoft.odb;
+
+import java.io.Serializable;
+
+/**
+ * <p>Title: odb</p>
+ * <p>Description: Simple Object Database</p>
+ * <p>Copyright: Copyright (c) 2004</p>
+ * <p>Company: </p>
+ * @author not attributable
+ * @version 1.0
+ */
+
+public class ObjectRefKey implements Comparable, Serializable
+{
+	private Comparable m_object_key;
+    public int compareTo(Object parm1)
+	{
+		ObjectRefKey  to_compare=(ObjectRefKey)parm1;
+		if ( m_object_key==null)
+		{
+			if ( to_compare.m_object_key==null)
+				return 0;
+			return -1;
+		}
+		if ( to_compare.m_object_key==null)
+			return 1;
+		return m_object_key.compareTo( to_compare.m_object_key);
+	}
+
+	public ObjectRefKey( ObjectRef ref)
+	{
+		if ( ref.impl!=null)
+			m_object_key=(Comparable)ref.impl.objectKey;
+		else
+			m_object_key=null;
+	}
+}
