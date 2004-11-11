6a7dc68a8314f50e2095e2610291c905fc2d5879
Michael A. MacDonald <michaelm@antlersoft.com>
2004-11-11 21:18:55 +0000

    Complete implementation of SymbolFinder

---
+++ eclipse-projects/bbq_eclipse/src/com/antlersoft/analyzecxx/SymbolFinder.java
@@ -0,0 +1,88 @@
+package com.antlersoft.analyzecxx;
+
+import com.antlersoft.parser.Symbol;
+
+/**
+ * This class traverses a SymbolFinderTree one character at a time
+ * to (efficiently?) identify a symbol
+ */
+public class SymbolFinder
+{
+	private SymbolFinderTree m_tree;
+	private SymbolFinderTree.Node m_node;
+	private Symbol m_last_symbol;
+	private StringBuffer m_remainder;
+
+	public SymbolFinder( SymbolFinderTree tree)
+	{
+		m_tree=tree;
+		m_remainder=new StringBuffer( 20);
+		reset();
+	}
+
+	/**
+	 * See if the next character can move you to a new node in the tree.  If
+	 * the new node matches a symbol, the function returns true and the
+	 * remainder is cleared.  If the new node does not match a symbol,
+	 * the function returns true and the character is added to the remainder.
+	 * If no new node is available, the function returns false and
+	 * the character is added to the remainder.
+	 */
+	public final boolean accept( char c)
+	{
+		SymbolFinderTree.Node new_node=m_node.nextNode( c);
+		if ( new_node==null)
+		{
+			m_remainder.append( c);
+			return false;
+		}
+		else
+		{
+			if ( new_node.m_symbol!=null)
+			{
+				m_last_symbol=new_node.m_symbol;
+				m_remainder.setLength( 0);
+			}
+			else
+			{
+				m_remainder.append( c);
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Return false if the current node can not accept any other characters
+	 */
+	public final boolean canGrow()
+	{
+		return m_node.canGrow();
+	}
+
+	/**
+	 * Returns the last recognized symbol
+	 */
+	public final Symbol currentSymbol()
+	{
+		return m_last_symbol;
+	}
+
+	/**
+	 * Resets to the root of the tree
+	 */
+	public final void reset()
+	{
+		m_last_symbol=null;
+		m_node=m_tree.m_root;
+		m_remainder.setLength( 0);
+	}
+
+	/**
+	 * Returns the characters that have been accepted since the
+	 * last recognized symbol
+	 */
+	public final String getRemainder()
+	{
+		return m_remainder.toString();
+	}
+}
---
+++ eclipse-projects/bbq_eclipse/src/com/antlersoft/analyzecxx/SymbolFinderTree.java
@@ -0,0 +1,90 @@
+package com.antlersoft.analyzecxx;
+
+import java.util.Iterator;
+import java.util.TreeMap;
+
+import com.antlersoft.parser.Symbol;
+
+public class SymbolFinderTree
+{
+	static class Node
+	{
+		Symbol m_symbol;
+		Link[] m_links;
+
+		final boolean canGrow()
+		{
+			return m_links.length>0;
+		}
+
+		final Node nextNode( char c)
+		{
+			for ( int i=0; i<m_links.length; ++i)
+			{
+				if ( c==m_links[i].m_match)
+					return m_links[i].m_next;
+				if ( c>m_links[i].m_match)
+					break;
+			}
+			return null;
+		}
+	}
+
+	static class BuildNode
+	{
+		Symbol m_symbol;
+		TreeMap m_tree;
+
+		BuildNode()
+		{
+			m_tree=new TreeMap();
+		}
+
+		Node makeNode()
+		{
+			Node result=new Node();
+			result.m_symbol=m_symbol;
+			result.m_links=new Link[m_tree.size()];
+			int i=0;
+			for ( Iterator it=m_tree.entrySet().iterator(); it.hasNext(); ++i)
+			{
+				TreeMap.Entry entry=(TreeMap.Entry)it.next();
+				result.m_links[i].m_match=((Character)entry.getKey()).charValue();
+				result.m_links[i].m_next=((BuildNode)entry.getValue()).makeNode();
+			}
+			return result;
+		}
+	}
+
+	static class Link
+	{
+		char m_match;
+		Node m_next;
+	}
+
+	Node m_root;
+
+	public SymbolFinderTree( String[] to_find)
+	{
+		BuildNode root=new BuildNode();
+		for ( int i=0; i<to_find.length; ++i)
+		{
+			String s=to_find[i];
+			int l=s.length();
+			BuildNode current=root;
+			for ( int j=0; j<l; ++j)
+			{
+				Character c=new Character( s.charAt( j));
+				BuildNode next=(BuildNode)current.m_tree.get( c);
+				if ( next==null)
+				{
+					next=new BuildNode();
+					current.m_tree.put( c, next);
+				}
+				current=next;
+			}
+			current.m_symbol=Symbol.get( s);
+		}
+		m_root=root.makeNode();
+	}
+}
