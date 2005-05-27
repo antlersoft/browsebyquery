c6b8cef226fe16b61ca36f0b9291bbfcb3523c54
Michael A. MacDonald <michaelm@antlersoft.com>
2005-05-27 18:30:08 +0000

    Add copyright notice

--- javagen/javagen.cpp
+++ javagen/javagen.cpp
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 1999-2005  Michael A. MacDonald
+ * ----- - - -- - - --
+ *     This file is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This file is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with the file (see COPYING); if not, see www.gnu.org
+ * ----- - - -- - - --
+ */
 #include "com/antlersoft/parser/BuildParse.h"
 #include "com/antlersoft/parser/LexScan.h"
 #include "com/antlersoft/parser/SeqStr.h"
