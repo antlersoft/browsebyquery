8fac423540fae2ff7e671c2e416a4abce7baa295
Michael A. MacDonald <michaelm@antlersoft.com>
2006-03-14 19:11:34 +0000

    Fix footer so sourceforge logo is correct

--- eclipse-projects/docs/querydoc/html/doit.sh
+++ eclipse-projects/docs/querydoc/html/doit.sh
@@ -13,10 +13,9 @@ print <<EOF
                 <tr>
                   <td valign="center"><a href="http://browsebyquery.sourceforge.net">browse-by-query home</a>             </td>
                <td valign="center" align="center">   <a href="http://www.antlersoft.com/freesoftwarenews.html">antlersoft free software</a>          </td>
-		<td align="right" valign="center">
-<a href="http://sourceforge.net/projects/browsebyquery" target="_top" valign="center">project page</a> 
-<a href="http://sourceforge.net/projects/browsebyquery" target="_top" valign=
-"center"><span class="inlinemediaobject"><img src="http://sourceforge.net/sflogo.php?group_id=139907&amp;type=1" height="31" width="88" longdesc="ld-id2764673.html"></span></a>
+		<td align="center" valign="center">
+<a href="http://sourceforge.net/projects/browsebyquery" target="_top" valign="center">sourceforge project page</a> </td><td align="right">
+<a href="http://sourceforge.net" target="_top"><span class="inlinemediaobject"><img src="http://sourceforge.net/sflogo.php?group_id=139907&amp;type=1" height="31" width="88" longdesc="ld-id2764673.html"></span></a>
 				</td>
                 </tr>
                                                
