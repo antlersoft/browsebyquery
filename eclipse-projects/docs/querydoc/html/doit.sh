e4409508a85734b57ddf6b989a31197f1a2a07fd
Michael A. MacDonald <michaelm@antlersoft.com>
2006-04-29 02:58:57 +0000

    Change to use docbook xsl and toc settings

--- eclipse-projects/docs/querydoc/html/doit.sh
+++ eclipse-projects/docs/querydoc/html/doit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-xsltproc /opt/kde/share/apps/ksgmltools2/docbook/xsl/html/chunk.xsl $1
+#xsltproc /usr/kde/3.4/share/apps/ksgmltools2/docbook/xsl/html/chunk.xsl $1
+xsltproc --param generate.section.toc.level 1 /usr/share/sgml/docbook/xsl-stylesheets-1.68.1/html/chunk.xsl $1
 
 perl -pi -e '
 if ( /^(.*)(\<\/body\>.*)/ )
