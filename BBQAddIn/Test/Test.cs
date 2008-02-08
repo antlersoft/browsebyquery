28c5b16b518a27dfe89b961f25dd0028fff4bee8
Michael A. MacDonald <michaelm@antlersoft.com>
2008-02-08 18:45:41 +0000

    move test.cs?

---
+++ BBQAddIn/Test/Test.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace com.antlersoft.BBQAddIn
+{
+	class Test
+	{
+		public static void Main( String[] args)
+		{
+			Object o=new BrowseByQueryBySocket().PerformQuery(
+				new QueryRequest("methods in class \"System.Int32\""));
+			StringWriter sw=new StringWriter();
+			new XmlSerializer(typeof(QueryResponse)).Serialize( sw, o);
+			Console.WriteLine( sw.ToString());
+			Console.WriteLine( "Done!");
+		}
+	}
+}
