a836a12aa4ebb6d9b6586aef0ff06d5cc6d3abc0
Michael A. MacDonald <michaelm@antlersoft.com>
2008-03-25 23:35:34 +0000

    Add doc comments

--- eclipse-projects/bbq_eclipse/src/com/antlersoft/util/xml/IElement.java
+++ eclipse-projects/bbq_eclipse/src/com/antlersoft/util/xml/IElement.java
@@ -15,8 +15,25 @@ import org.xml.sax.helpers.DefaultHandler;
  */
 public interface IElement {
 
+	/**
+	 * Return an object that can be pushed on the stack and used to read this element
+	 * (and any elements it contains)
+	 * @param handler_stack Stack of handlers used to read enclosing elements
+	 * @return Subclass of DefaultHandler suitable for reading this element
+	 */
     public DefaultHandler readFromXML( IHandlerStack handler_stack);
+    /**
+     * Write this element to a ContentHandler so that it might be read back
+     * with the handler returned by readFromXML
+     * @param xml_writer 
+     * @throws SAXException Thrown from ContentHandler methods or other problem
+     * writing out object
+     */
 	public void writeToXML( ContentHandler xml_writer)
 		throws SAXException;
+	/**
+	 * Return the element name of the elements that will be used to store this class
+	 * @return Element name
+	 */
 	public String getElementTag();
 }
