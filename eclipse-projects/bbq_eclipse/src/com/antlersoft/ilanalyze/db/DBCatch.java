39a49592d848a30578ba669686cd97767dc3ffb7
Michael A. MacDonald <michaelm@antlersoft.com>
2008-11-13 20:55:40 +0000

    Capture info about catches

---
+++ eclipse-projects/bbq_eclipse/src/com/antlersoft/ilanalyze/db/DBCatch.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2008 Michael A. MacDonald
+ */
+package com.antlersoft.ilanalyze.db;
+
+import com.antlersoft.odb.ObjectDB;
+
+/**
+ * @author Michael A. MacDonald
+ *
+ */
+public class DBCatch extends DBReference<DBClass> {
+	final static String CATCH_TARGET="CATCH_TARGET";
+	DBCatch( DBMethod source, DBClass target)
+	{
+		super(source,target);
+		ObjectDB.makePersistent(this);
+	}
+	
+	public DBClass getCaught()
+	{
+		return m_target.getReferenced();
+	}
+	public String toString()
+	{
+		StringBuilder sb=new StringBuilder();
+		
+		sb.append( "Catch of ");
+		sb.append( getCaught().toString());
+		addFromInfo( sb );
+		
+		return sb.toString();
+	}
+}
