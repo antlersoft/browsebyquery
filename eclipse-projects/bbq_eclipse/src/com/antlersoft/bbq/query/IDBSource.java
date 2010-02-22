f2a2af895545b052c802dce867f8e634cd6d9de1
Michael A. MacDonald <mars@antlersoft.com>
2010-02-22 11:21:33 -0500

    Fix typo in javadoc

--- eclipse-projects/bbq_eclipse/src/com/antlersoft/bbq/query/IDBSource.java
+++ eclipse-projects/bbq_eclipse/src/com/antlersoft/bbq/query/IDBSource.java
@@ -8,7 +8,7 @@ import com.antlersoft.odb.IndexObjectDB;
 import com.antlersoft.query.DataSource;
 
 /**
- * A DataSource bases on an IndexObjectDB.
+ * A DataSource based on an IndexObjectDB.
  * <p>
  * This interface provides a method for accessing the underlying IndexObjectDB.
  * @author Michael A. MacDonald
