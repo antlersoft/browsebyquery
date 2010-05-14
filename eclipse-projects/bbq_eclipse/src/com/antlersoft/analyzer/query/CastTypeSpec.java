865f2ec8cd280e6f8633c9dc2e8b7a94c4fbdb76
Michael A. MacDonald <mars@antlersoft.com>
2010-05-14 18:14:10 -0400

    Update to remove unchecked warning

--- eclipse-projects/bbq_eclipse/src/com/antlersoft/analyzer/query/CastTypeSpec.java
+++ eclipse-projects/bbq_eclipse/src/com/antlersoft/analyzer/query/CastTypeSpec.java
@@ -16,8 +16,8 @@ import com.antlersoft.analyzer.DBMethod;
  */
 class CastTypeSpec {
 	private static String[] names={ "arguments","classes","fields","methods" };
-	private static Class[] classes={ DBArgument.class, DBClass.class, DBField.class, DBMethod.class };
-	public static Class getClassFor( String f)
+	private static Class<?>[] classes={ DBArgument.class, DBClass.class, DBField.class, DBMethod.class };
+	public static Class<?> getClassFor( String f)
 	{
 		for ( int i=0; i<names.length; ++i)
 			if ( names[i].equals(f))
